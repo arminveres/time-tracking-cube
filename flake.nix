0916c7792be443eefb4ff07b023759a847ee9f0d
Armin Veres <armin.veres@hotmail.com>
2024-04-22 18:25:57 +0200

    feat: add further flake dependencies and add darwin aarch64

--- flake.nix
+++ flake.nix
@@ -2,41 +2,39 @@
   description = "Time-Tracking Cube (TTC)";
 
   inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/unstable";
-    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    # nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
-    # Add the system/architecture you would like to support here. Note that not
-    # all packages in the official nixpkgs support all platforms.
+  # Add the system/architecture you would like to support here. Note that not
+  # all packages in the official nixpkgs support all platforms.
+  outputs = inputs@{ self, nixpkgs, ... }: inputs.utils.lib.eachSystem [
     "x86_64-linux"
+    "aarch64-darwin"
   ]
     (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-
-          # Add overlays here if you need to override the nixpkgs
-          # official packages.
           overlays = [ ];
-
-          # Uncomment this if you need unfree software (e.g. cuda) for
-          # your project.
-          #
           config.allowUnfree = true;
         };
       in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = pkgs.mkShellNoCC {
           packages = with pkgs; [
+            zsh
             # Development Tools
             cppcheck
-            gcc-arm-embedded
+            gcc-arm-embedded-13
             cmake
             gnumake
-            zsh
+            picotool
+            minicom
           ];
+          shellHook = ''
+          '';
         };
       });
 }
